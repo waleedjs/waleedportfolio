00559e65e9df8995a64d0d50d0fd7ab47197d725
waleedjs <128383681+waleedjs@users.noreply.github.com>
2024-07-09 10:07:17 +0500

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
                 </div>
                 <div className="flex-1">
                   <p className="text-white/60">{item.title}</p>
-                  <h3 className="text-xl">{item.description}</h3>
+                  <h3 className="xl:text-xl text-sm">{item.description}</h3>
                 </div>
               </li>
             })}
